6a93d60742570f3e80b1608d48824579f3681926
saifalgiri@gmail.com <34902733+saifalgiri@users.noreply.github.com>
2019-01-17 17:08:47 +0800

    Add project files.

---
+++ DesignPattern/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace DesignPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singletone staff = Singletone.SingleInstance;
+            staff.LogMessage("staff");
+
+            Singletone manager = Singletone.SingleInstance;
+            manager.LogMessage("manager");
+            Console.ReadKey();
+        }
+    }
+
+    public class Singletone
+    {
+        static int Counter = 0;
+        private static Singletone singleInstance = null;
+
+        private  Singletone()
+        {
+            Counter++;
+            Console.WriteLine("Instance created {0}", Counter);
+        }
+
+        public static Singletone SingleInstance
+        {
+            get
+            {
+                if(singleInstance == null)
+                {
+                    singleInstance = new Singletone();
+                }
+
+                return singleInstance;
+            }
+        }
+
+        public void LogMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+
+}
